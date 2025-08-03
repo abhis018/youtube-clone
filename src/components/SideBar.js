b008e0ed6e133560ea552e5ac7dfe8aead2f2325
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-03 23:53:51 +0530

    creadted side bar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,47 @@
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+import { SiYoutubemusic } from "react-icons/si";
+import { AiOutlineHistory } from "react-icons/ai";
+import { MdOutlinePlaylistPlay } from "react-icons/md";
+
+const SideBar = () => {
+  return (
+    <div>
+      <div>
+        <IoMdHome/>
+        <p>Home</p>
+      </div>
+      <div><SiYoutubeshorts /><p>Shorts</p></div>
+      <div><MdSubscriptions /><p>Subscription</p></div>
+      <div><SiYoutubemusic /><p>YouTube Music</p></div>
+      <div><AiOutlineHistory /><p>History</p></div>
+      <div><MdOutlinePlaylistPlay /><p>Playlists</p></div>
+      <div><p>Your videos</p></div>
+      <div><p>Watch Later</p></div>
+      <div><p>Liked videos</p></div>
+      <div><p>Watch Later</p></div>
+      <div><p>Liked videos</p></div>
+      <div><p>Downloads</p></div>
+      <div><p>Shoping</p></div>
+      <div><p>Music</p></div>
+      <div><p>Flims</p></div>
+      <div><p>Live</p></div>
+      <div><p>Gaming</p></div>
+      <div><p>News</p></div>
+      <div><p>Sport</p></div>
+      <div><p>Courses</p></div>
+      <div><p>Fashion Beauty</p></div>
+      <div><p>Podcasts</p></div>
+      <div><p>Youtube Studio</p></div>
+      <div><p>Youtube Music</p></div>
+      <div><p>Youtube Kids</p></div>
+      <div><p>Settings</p></div>
+      <div><p>Report history</p></div>
+      <div><p>Help</p></div>
+      <div><p>Send feedback</p></div>
+    </div>
+  )
+}
+
+export default SideBar
