a0ad01081c18906820bd5dc599c4b671e84a135c
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-03 23:43:39 +0530

    created tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        custom: '#f8f8f8',
+      },
+    },
+  },
+  plugins: [],
+}
+
