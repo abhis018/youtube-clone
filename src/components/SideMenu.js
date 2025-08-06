e7d9f9e0556b67580334df04b0a5a964b544a7ca
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-06 23:30:28 +0530

    important side menu exist

---
+++ src/components/SideMenu.js
@@ -0,0 +1,39 @@
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+import { SiYoutubemusic } from "react-icons/si";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { FaRegCircleUser } from "react-icons/fa6";
+
+const SideMenu = () => {
+  return (
+    <div className="flex flex-col items-center">
+        <div className="pt-8 flex flex-col items-center">
+            <IoMdHome className="text-2xl"/>
+            <p className="text-xs text-center">Home</p>
+        </div>
+        <div className="pt-8 flex flex-col items-center">
+            <SiYoutubeshorts className="text-2xl" />
+            <p className="text-xs text-center">Shorts</p>
+        </div>
+        <div className="pt-8 flex flex-col items-center">
+            <MdSubscriptions className="text-2xl" />
+            <p className="text-xs text-center">Subscription</p>
+        </div>
+        <div className="pt-8 flex flex-col items-center">
+            <SiYoutubemusic  className="text-2xl"/>
+            <p className="text-xs text-center">YouTube Music</p>
+        </div>
+        <div className="pt-8 flex flex-col items-center">
+            <FaRegCircleUser className="text-2xl" />
+            <p className="text-xs text-center">You</p>
+        </div>
+        <div className="pt-8 flex flex-col items-center">
+            <MdOutlineFileDownload className="text-2xl" />
+            <p className="text-xs text-center">Downloads</p>
+        </div>
+    </div>
+  )
+}
+
+export default SideMenu
