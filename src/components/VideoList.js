7a2055579f2617b802333c4e6fd702581ac8c312
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-06 23:31:06 +0530

    fetch the data from the youtube api

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { YOUTUBE_VIDEOS_API } from '../utils/Constants';
+import VideoCard from './VideoCard';
 
 const VideoList = () => {
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    getVideos();
+  },[])
+
+  
+  const getVideos = async () => {
+    try {
+      const data = await fetch(YOUTUBE_VIDEOS_API);
+      const json = await data.json();
+      console.log(json);
+
+      if (json?.items?.length) {
+        setVideos(json.items);
+      } else {
+        console.warn("No videos found in response", json);
+      }
+    } catch (error) {
+      console.error("Failed to fetch videos:", error);
+    }
+  };
   return (
-    <div>VideoList</div>
+    <div className='flex flex-wrap'>
+      {videos.map((video => <VideoCard  key={video.id} info={video}/>))}
+    </div>
   )
 }
 
