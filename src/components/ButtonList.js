fc6e5f48bbfdfebd1d68b3769e0e9e693a2c3ebb
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-06 23:28:58 +0530

    created multiple button component using different name

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import Button from "./Button"
 
 const ButtonList = () => {
+
+  const list = ["All", "Music", "Cricket", "Comedy", "Trailers", "Movie", "Game", "Live", "Albums", "Recently Watched", "Recently uploaded", "News"];
+
   return (
-    <div>ButtonList</div>
+    <div className='flex overflow-x-auto whitespace-nowrap'>
+      <Button name="All"/>
+      <Button name="Music"/>
+      <Button name="Cricket"/>
+      <Button name="Comedy"/>
+      <Button name="Trailers"/>
+      <Button name="Movie"/>
+      <Button name="Game"/>
+      <Button name="Live"/>
+      <Button name="Albums"/>
+      <Button name="Recently Watched"/>
+      <Button name="Recently Uploaded"/>
+      <Button name="News"/>
+    </div>
   )
 }
 
