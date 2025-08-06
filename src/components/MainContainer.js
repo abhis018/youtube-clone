37e72532235a725a7de7f7d0c682a389d145055c
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-06 23:29:45 +0530

    created main container where sidemenu buttonlist and videolist exist

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ButtonList from './ButtonList'
 import VideoList from './VideoList'
+import SideMenu from './SideMenu'
 
 const MainContainer = () => {
   return (
-    <div>
+    <div className='flex'>
+      <div>
+        <SideMenu/>
+      </div>
+      <div>
         <ButtonList/>
         <VideoList/>
+      </div>
     </div>
   )
 }
