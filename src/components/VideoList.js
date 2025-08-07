4e47ede2aabdf9af7eb1c0e28aef1d006b239bf1
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-07 23:11:40 +0530

    changes in the map section

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from '../utils/Constants';
 import VideoCard from './VideoCard';
+import { Link } from 'react-router-dom';
 
 const VideoList = () => {
   const [videos, setVideos] = useState([])
@@ -26,7 +27,9 @@ const VideoList = () => {
   };
   return (
     <div className='flex flex-wrap'>
-      {videos.map((video => <VideoCard  key={video.id} info={video}/>))}
+      {videos.map((video) => (
+        <Link to={"/watch?v=" + video.id}><VideoCard  key={video.id} info={video}/></Link>
+      ))}
     </div>
   )
 }
