b9e30fb45bf39af1f31083d08f58997061e8b4e7
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-13 21:45:13 +0530

    delete the console log

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -14,7 +14,6 @@ const VideoList = () => {
     try {
       const data = await fetch(YOUTUBE_VIDEOS_API);
       const json = await data.json();
-      console.log(json);
 
       if (json?.items?.length) {
         setVideos(json.items);
