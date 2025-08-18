6f30dd19580a16b1fbfd54867f745ac1c9fd0f79
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-18 23:37:03 +0530

    added comment section in the watch page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import { closeMenu } from "../utils/appSlice";
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
 
@@ -13,16 +14,19 @@ const WatchPage = () => {
   // }, []);
 
   return (
-    <div className='ml-12 mt-5'>
-      <iframe className="rounded-xl" 
-        width="750" 
-        height="400" 
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")} 
-        title="YouTube video player" 
-        frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
-        allowFullScreen>
-        </iframe>
+    <div className='flex flex-col'>
+      <div className='ml-12 mt-5'>
+        <iframe className="rounded-xl" 
+          width="750" 
+          height="400" 
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")} 
+          title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
+          allowFullScreen>
+          </iframe>
+      </div>
+      <CommentsContainer/>
     </div>
   )
 }
