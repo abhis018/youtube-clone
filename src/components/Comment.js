beffdd2aaa4bc5d36869ba03ea65209262b378e7
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-19 23:01:11 +0530

    changes the size of the icon and the dynamic name and comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FaCircleUser } from "react-icons/fa6";
 
-const Comment = (data) => {
+const Comment = ({data}) => {
     const {name, text, replies} = data;
   return (
-    <div>
-        <FaCircleUser size={25} />
-        <div>
-            <p>Name</p>
-            <p>Comment</p>
+    <div className='flex bg-gray-100 shadow-sm p-2 rounded-lg my-2'>
+        <FaCircleUser size={50} />
+        <div className='px-3'>
+            <p>{name}</p>
+            <p>{text}</p>
         </div>
     </div>
   )
