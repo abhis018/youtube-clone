3c84b2318293904c498c473de806db2fa88a39d2
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-02 22:41:44 +0530

    changes the styling

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import { closeMenu } from "../utils/appSlice";
 import CommentsContainer from './CommentsContainer';
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
 
@@ -14,17 +15,22 @@ const WatchPage = () => {
   // }, []);
 
   return (
-    <div className='flex flex-col'>
-      <div className='ml-12 mt-5'>
-        <iframe className="rounded-xl" 
-          width="750" 
-          height="400" 
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")} 
-          title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
-          allowFullScreen>
-          </iframe>
+    <div className='flex flex-col w-full'>
+      <div className='ml-12 mt-5 flex'>
+        <div>
+          <iframe className="rounded-xl" 
+            width="750" 
+            height="400" 
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")} 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
+            allowFullScreen>
+            </iframe>
+          </div>
+          <div className='w-full'>
+            <LiveChat/>
+          </div>
       </div>
       <CommentsContainer/>
     </div>
