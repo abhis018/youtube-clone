e837cd29ae061f305d330f7922f1f82cc9585001
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-03 23:34:16 +0530

    delete unwanted imports

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+
 import { useSearchParams } from 'react-router-dom';
-import { closeMenu } from "../utils/appSlice";
 import CommentsContainer from './CommentsContainer';
 import LiveChat from "./LiveChat";
 
