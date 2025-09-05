ba80bec8bd5f4e1b389ebed9501fe3128840d407
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-05 23:30:57 +0530

    added a function with random name for the live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ChatMessage from './ChatMessage';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../utils/chatSlice';
+import { generateRandomName } from '../utils/helper';
+
 
 const LiveChat = () => {
 
   const dispatch = useDispatch();
+  const chatMessages = useSelector(store => store.chat.messages)
   useEffect(()=>{
     const i = setInterval(()=>{
       console.log("Api Polling");
       dispatch(addMessage({
-        name: "abhishek kumar",
+        name: generateRandomName(),
         message: "This is the youtube-clone app"
       }))
     },2000);
@@ -19,10 +22,11 @@ const LiveChat = () => {
   },[]);
 
   return (
-    <div className='w-full h-[400px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
-        <ChatMessage name="Abhishek kumar" message="This is youtube-clone app!"/>
+    <div className='w-full h-[400px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll'>
+        {//Disclaimer: don't use index as a key
+          chatMessages.map((c, i)=> <ChatMessage key={i} name={c.name} message={c.message}/>)}
     </div>
   )
 }
 
-export default LiveChat;
+export default LiveChat;  
