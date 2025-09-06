67a65869fbc75d3e117549abf1c0e32869bfb5fd
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-06 23:10:15 +0530

    added a function for the random message

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -25,3 +25,14 @@
 export function generateRandomName() {
     return nameList[Math.floor( Math.random() * nameList.length )];
 };
+
+
+export function makeRandomMessage(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
