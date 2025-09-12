623aad4639991f2a721886e84050f0df132df649
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-12 23:35:54 +0530

    use the state to change the text in the input field

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo = () => {
+    const [text, setText] = useState("");
   return (
-    <div className='m-2 p-2 w-96 h-96 border border-black'>Demo</div>
+    <div className='m-2 p-2 w-96 h-96 border border-black'>
+        <div className='border border-black w-72'>
+            <input type='text' value={text} onChange={(e) => setText(e.target.value)}/>
+        </div>
+    </div>
   )
 }
 
