b1e5033bfe6833bf73c3ef0ba8251e6a522418ea
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-13 00:00:02 +0530

    prime number function added

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -36,3 +36,30 @@ export function makeRandomMessage(length) {
     }
     return result;
 }
+
+
+export function findPrime(num) {
+   if (num <= 1) {
+     return false;
+   }
+   for (let i = 2; i <= Math.sqrt(num); i++) {
+     if (num % i === 0) {
+      return false;
+     }
+   }
+   return true;
+  }
+ 
+  //Function to find the nth prime number
+  function findNthPrime(n) {
+   let count = 0;
+   let num = 2;
+   while (count < n) {
+     if (findPrime(num)) {
+      count++;
+     }
+     num++;
+   }
+   return num - 1;
+  }
+ 
