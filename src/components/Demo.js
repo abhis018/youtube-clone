c02a6d045c8f9a639cf7ecf8778d0fab08507113
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-13 00:00:23 +0530

    display prime number in the ui

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {findPrime} from "../utils/helper"
 
 const Demo = () => {
-    const [text, setText] = useState("");
+    const [text, setText] = useState(0);
+
+    const prime = findPrime(text);
   return (
     <div className='m-2 p-2 w-96 h-96 border border-black'>
-        <div className='border border-black w-72'>
-            <input type='text' value={text} onChange={(e) => setText(e.target.value)}/>
+        <div >
+            <input className='border border-black w-72 px-2' type='number' value={text} onChange={(e) => setText(e.target.value)}/>
+        </div>
+        <div>
+            <h1>nth prime: {prime}</h1>
         </div>
     </div>
   )
