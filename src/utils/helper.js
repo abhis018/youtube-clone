00d3be6979a233fa73f23d88dd49bdbc925a5b5f
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-13 15:25:44 +0530

    creating a function to print the prime number without rendering

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -38,28 +38,28 @@ export function makeRandomMessage(length) {
 }
 
 
-export function findPrime(num) {
-   if (num <= 1) {
-     return false;
-   }
-   for (let i = 2; i <= Math.sqrt(num); i++) {
-     if (num % i === 0) {
-      return false;
-     }
-   }
-   return true;
-  }
+
  
-  //Function to find the nth prime number
-  function findNthPrime(n) {
-   let count = 0;
-   let num = 2;
-   while (count < n) {
-     if (findPrime(num)) {
+ 
+ function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+
+export function findPrime(n) {
+  let count = 0;
+  let num = 2;
+
+  while (count < n) {
+    if (isPrime(num)) {
       count++;
-     }
-     num++;
-   }
-   return num - 1;
+    }
+    num++;
   }
- 
+
+  return num - 1; 
+}
