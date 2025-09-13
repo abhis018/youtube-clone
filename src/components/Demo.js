8fd762189dbd511d70e7c7ba8a6b48d8f0429bef
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-09-13 15:25:19 +0530

    created a state for changing the theme

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import {findPrime} from "../utils/helper"
 
 const Demo = () => {
     const [text, setText] = useState(0);
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
 
-    const prime = findPrime(text);
+    const prime = useMemo(()=>findPrime(text),[text]) ;
   return (
-    <div className='m-2 p-2 w-96 h-96 border border-black'>
+    <div className={'m-2 p-2 w-96 h-96 border border-black' + (isDarkTheme && " bg-gray-900 text-white")}>
         <div >
             <input className='border border-black w-72 px-2' type='number' value={text} onChange={(e) => setText(e.target.value)}/>
+        </div>
+        <div>
+            <button className='bg-green-200' onClick={() => setIsDarkTheme(!isDarkTheme)}>Toggle</button>
         </div>
         <div>
             <h1>nth prime: {prime}</h1>
